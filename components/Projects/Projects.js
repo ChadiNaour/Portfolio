c2f8de55fa267bcd9e90d398b946ce5486c8b129
ChadiNaour <naourchadi@gmail.com>
2022-09-04 22:01:52 +0100

    fixing projects card styling in mobile display

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -82,7 +82,7 @@ const ProjectsSection = ({ }) => {
                     userSelect: "none",
                 }}
             >PROJECTS</div>
-            <div className='block absolute z-0 mx-auto p-0 font-timmons md:text-6xl sm:hidden'
+            <div className='hidden md:block absolute z-0 mx-auto p-0 font-timmons md:text-6xl sm:hidden'
                 style={{
                     fontSize: "calc(var(--vh, 1vh) * 20)",
                     color: "#181818",
@@ -94,11 +94,23 @@ const ProjectsSection = ({ }) => {
                     userSelect: "none",
                 }}
             >PROJECTS</div>
+            <div className='block absolute z-0 mx-auto p-0 font-timmons md:text-6xl sm:hidden'
+                style={{
+                    fontSize: "calc(var(--vh, 1vh) * 90)",
+                    color: "#181818",
+                    left: "50%",
+                    top: "50%",
+                    transform: "translate(-50%, -50%)",
+                    transition: "0.5s opacity",
+                    willChange: "transform",
+                    userSelect: "none",
+                }}
+            >PR</div>
             <Swiper className="mySwiper cursor-grab">
                 {Projects.map((project, index) => (
                     <SwiperSlide key={index}>
                         <div className={`w-screen h-full flex justify-center z-20 items-center`}>
-                            <div className='w-[90%] xs:w-[25rem] h-[40rem] md:w-[90vw] lg:w-3/4 xl:w-3/5 md:h-[27rem] lg:h-[27rem] xl:h-[27rem] 2xl:h-[30rem] 3xl:w-1/2 3xl:h-[49rem] z-20 rounded-md md:rounded-sm relative flex flex-col items-center shadow-2xl bg-[#2B2B2B]' key={index}>
+                            <div className='w-[90%] xs:w-[22rem] h-[38rem] md:w-[90vw] lg:w-3/4 xl:w-3/5 md:h-[27rem] lg:h-[27rem] xl:h-[27rem] 2xl:h-[30rem] 3xl:w-1/2 3xl:h-[49rem] z-20 rounded-md md:rounded-sm relative flex flex-col items-center shadow-2xl bg-[#2B2B2B]' key={index}>
                                 <div className="relative h-[10%] md:h-1/6 w-full">
                                     <div className="absolute top-0 right-0 mt-7 mr-4 md:mr-7 xl:mr-10"><span className='text-white text-xl xl:text-2xl font-reloadLight'>{index + 1} / 5</span></div>
                                 </div>
@@ -125,8 +137,8 @@ const ProjectsSection = ({ }) => {
                                     </div>
                                 </div>
                                 {/* buttons holder */}
-                                <div className='h-[15%] md:h-1/6 w-full flex justify-center flex-row md:justify-end'>
-                                    <div className="h-full  md:w-1/2 xl:w-1/3 flex md:justify-end items-center md:items-center xl:items-start  mx-4  md:mr-7 xl:mr-10">
+                                <div className='h-[15%]  md:h-1/6 w-full flex justify-center flex-row md:justify-end'>
+                                    <div className="h-full  md:w-1/2 xl:w-1/3 flex md:justify-end items-center md:items-start  mx-4  md:mr-7 xl:mr-10">
                                         <a className={`${ProjectStyle.btn} ${ProjectStyle.btnCircle} h-10 w-28 sm:h-10 sm:w-32  xl:h-11 xl:w-40 2xl:h-12 2xl:w-44`} href={project.link} target="_blank">
                                             <svg xmlns="http://www.w3.org/2000/svg" className={`${ProjectStyle.Icon} w-[1.2rem] h-[1.2rem] mr-2`} viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" /></svg>
                                             <span className='cotet z-20 font-reloadLight mt-1 text-sm lg:text-[1rem] flex flex-row items-center'>repo</span>
@@ -149,7 +161,7 @@ const ProjectsSection = ({ }) => {
                 <span>© 2022 Designed & Developed Nchadi</span>
             </div>
             {/* </motion.main> */}
-            <div ref={ref1} className="absolute h-28 md:h-34 z-20 flex justify-center items-start left-0 bottom-0 w-full text-lg select-none" >
+            <div ref={ref1} className="absolute h-24 md:h-34 z-20 flex justify-center items-start left-0 bottom-0 w-full text-lg select-none" >
                 <div className="h-12 w-96   flex justify-start items-start">
                     <div className=" h-full z-10 w-2/5 bg-dark"></div>
                     <div className='flex flex-col  justify-center gap-3 items-start  h-full w-1/5'>
